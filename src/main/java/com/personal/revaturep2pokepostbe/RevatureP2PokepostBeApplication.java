bd366cd04ef5298c0c59a59961641839fff66bd6
BarritoN78 <95886922+BarritoN78@users.noreply.github.com>
2022-08-26 12:05:15 -0400

    Created the base maven project
    Used the spring boot initializer website: https://start.spring.io/ to save me some time

---
+++ src/main/java/com/personal/revaturep2pokepostbe/RevatureP2PokepostBeApplication.java
@@ -0,0 +1,13 @@
+package com.personal.revaturep2pokepostbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevatureP2PokepostBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevatureP2PokepostBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/revaturep2pokepostbe/RevatureP2PokepostBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.revaturep2pokepostbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevatureP2PokepostBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
